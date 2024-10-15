e3ddfe8e996a5007a319d46d269443109397b3be
Gino Baptista <gb.g@live.com>
2024-10-15 10:49:36 -0400

    correccion en parte diaria y ordenamiento

--- src/web/finanzas/controller/finanzas.controller.js
+++ src/web/finanzas/controller/finanzas.controller.js
@@ -9,59 +9,66 @@ const parteDiaroController = async (req, res) => {
 
     const fechaFiltro = new Date(fecha).toISOString().split('T')[0];
     const result = await parteDiario();
+
     const resultadosFiltrados = result.filter(item => {
       const fechaItem = new Date(item.Fecha).toISOString().split('T')[0];
       return fechaItem === fechaFiltro;
     });
 
-    // Agrupar por Sección y crear la lista de descripciones
+    // Agrupar por sección y crear la lista de descripciones
     const agrupadoPorSeccion = resultadosFiltrados.reduce((acc, item) => {
       const seccion = item.Seccion;
 
-      // Si la sección no existe, iniciarla
       if (!acc[seccion]) {
         acc[seccion] = {
           totalIngreso: 0,
           totalEgreso: 0,
-          descripcion: []
+          descripcion: [],
         };
       }
 
-      // Agregar los ingresos y egresos a la sección correspondiente
       acc[seccion].totalIngreso += parseFloat(item.Ingreso);
       acc[seccion].totalEgreso += parseFloat(item.Egreso);
 
-      // Añadir el detalle específico a la lista de descripciones
-      acc[seccion].descripcion.push({
-        Cuenta: item.Cuenta,
-        Fecha: item.Fecha,
-        "Proveedor/Cliente": item['Proveedor/Cliente'],
-        Ingreso: item.Ingreso,
-        Egreso: item.Egreso,
-        Contracuenta: item.Contracuenta,
-        "Nombre de Cuenta": item['Nombre de Cuenta'],
-        Origen: item.Origen,
-        Descripción: item.Descripción,
-        Seccion: item.Seccion
-      });
+      if(seccion=='3. GASTO'){
+        acc[seccion].descripcion.push({
+          Cuenta: item.Cuenta,
+          Fecha: item.Fecha,
+          "Proveedor/Cliente": item['Proveedor/Cliente'],
+          Ingreso: item.Ingreso,
+          Egreso: item.Egreso,
+          Contracuenta: item.Contracuenta,
+          "Nombre de Cuenta": item['Nombre de Cuenta'],
+          Origen: item.Origen,
+          Descripción: item.Descripción,
+          Seccion: item.Seccion,
+        });
+      }
 
       return acc;
     }, {});
+
+    // Ordenar las secciones numéricamente o alfabéticamente
+    const seccionesOrdenadas = Object.keys(agrupadoPorSeccion)
+      .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }))
+      .reduce((obj, key) => {
+        obj[key] = agrupadoPorSeccion[key];
+        return obj;
+      }, {});
 
     // Calcular totales generales
     let totalGeneralIngreso = 0;
     let totalGeneralEgreso = 0;
 
-    // Recorrer todas las secciones para calcular el total general
-    for (const seccion in agrupadoPorSeccion) {
-      totalGeneralIngreso += agrupadoPorSeccion[seccion].totalIngreso;
-      totalGeneralEgreso += agrupadoPorSeccion[seccion].totalEgreso;
+    for (const seccion in seccionesOrdenadas) {
+      totalGeneralIngreso += seccionesOrdenadas[seccion].totalIngreso;
+      totalGeneralEgreso += seccionesOrdenadas[seccion].totalEgreso;
     }
 
     const resultadoFinal = {
-      agrupadoPorSeccion,
+      agrupadoPorSeccion: seccionesOrdenadas,
       totalGeneralIngreso,
-      totalGeneralEgreso
+      totalGeneralEgreso,
     };
 
     return res.status(200).json(resultadoFinal);
@@ -71,7 +78,90 @@ const parteDiaroController = async (req, res) => {
       error,
     });
   }
-}
+};
+
+
+const parteDiaroMesActualController = async (req, res) => {
+  try {
+    const result = await parteDiario();
+
+    // Obtener el mes y año actuales
+    const fechaActual = new Date();
+    const mesActual = fechaActual.getMonth(); // 0 = Enero, 1 = Febrero, etc.
+    const anioActual = fechaActual.getFullYear();
+
+    // Filtrar los resultados por el mes y año actuales
+    const resultadosFiltrados = result.filter(item => {
+      const fechaItem = new Date(item.Fecha);
+      const mesItem = fechaItem.getMonth();
+      const anioItem = fechaItem.getFullYear();
+      return mesItem === mesActual && anioItem === anioActual;
+    });
+
+    // Agrupar por sección y crear la lista de descripciones
+    const agrupadoPorSeccion = resultadosFiltrados.reduce((acc, item) => {
+      const seccion = item.Seccion;
+
+      if (!acc[seccion]) {
+        acc[seccion] = {
+          totalIngreso: 0,
+          totalEgreso: 0,
+          descripcion: [],
+        };
+      }
+
+      acc[seccion].totalIngreso += parseFloat(item.Ingreso);
+      acc[seccion].totalEgreso += parseFloat(item.Egreso);
+
+      if(seccion=='3. GASTO'){
+        acc[seccion].descripcion.push({
+          Cuenta: item.Cuenta,
+          Fecha: item.Fecha,
+          "Proveedor/Cliente": item['Proveedor/Cliente'],
+          Ingreso: item.Ingreso,
+          Egreso: item.Egreso,
+          Contracuenta: item.Contracuenta,
+          "Nombre de Cuenta": item['Nombre de Cuenta'],
+          Origen: item.Origen,
+          Descripción: item.Descripción,
+          Seccion: item.Seccion,
+        });
+      }
+      
+
+      return acc;
+    }, {});
+
+    // Ordenar las secciones numéricamente o alfabéticamente
+    const seccionesOrdenadas = Object.keys(agrupadoPorSeccion)
+      .sort((a, b) => a.localeCompare(b, undefined, { numeric: true }))
+      .reduce((obj, key) => {
+        obj[key] = agrupadoPorSeccion[key];
+        return obj;
+      }, {});
+
+    let totalGeneralIngreso = 0;
+    let totalGeneralEgreso = 0;
+
+    for (const seccion in seccionesOrdenadas) {
+      totalGeneralIngreso += seccionesOrdenadas[seccion].totalIngreso;
+      totalGeneralEgreso += seccionesOrdenadas[seccion].totalEgreso;
+    }
+
+    const resultadoFinal = {
+      agrupadoPorSeccion: seccionesOrdenadas,
+      totalGeneralIngreso,
+      totalGeneralEgreso,
+    };
+
+    return res.status(200).json(resultadoFinal);
+  } catch (error) {
+    return res.status(500).json({
+      mensaje: 'error en el controlador parteDiarioController',
+      error,
+    });
+  }
+};
 
 const abastecimientoController = async (req, res) => {
   try {
@@ -166,4 +256,5 @@ module.exports = {
   parteDiaroController,
   abastecimientoController,
   abastecimientoMesActualController,
+  parteDiaroMesActualController,
 }
--- src/web/finanzas/routes/finanzas.router.js
+++ src/web/finanzas/routes/finanzas.router.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
-const { parteDiaroController, abastecimientoController, abastecimientoMesActualController } = require('../controller/finanzas.controller')
+const { parteDiaroController, abastecimientoController, abastecimientoMesActualController, parteDiaroMesActualController } = require('../controller/finanzas.controller')
 const router = Router()
 
 router.post('/parte-diaria', parteDiaroController)
 router.post('/abastecimiento', abastecimientoController)
 router.get('/abastecimiento-mes-actual', abastecimientoMesActualController)
+router.get('/parte-diario-mes-actual', parteDiaroMesActualController)
 
 module.exports = router
