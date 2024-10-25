3fd29cb8aeff019e4632b76e927e3fc0a0723e44
Gino Baptista <gb.g@live.com>
2024-10-25 16:15:59 -0400

    calculo contable

--- src/web/contabilidad_module/controllers/contabilidad.controller.js
+++ src/web/contabilidad_module/controllers/contabilidad.controller.js
@@ -1,3 +1,4 @@
+const { tipoDeCambion } = require("./hana.controller")
 const { asientoContable } = require("./sld.controller")
 
 const asientoContableController = async (req, res) => {
@@ -11,6 +12,60 @@ const asientoContableController = async (req, res) => {
             Reference3,
             JournalEntryLines, } = req.body
 
+
+        console.log({
+            ReferenceDate,
+            DueDate,
+            Memo,
+            Reference,
+            Reference2,
+            Reference3,
+            JournalEntryLines,
+        })
+        const tipoCambio = await tipoDeCambion()
+        const usd = tipoCambio[0]
+        console.log({ usd })
+        const journalList = []
+        JournalEntryLines.map((item) => {
+            const { Credit, Debit, ...restData } = item
+            const cambio = +usd.Rate
+            if (Credit !== 0 && Debit == 0) {
+                const newValue = +Credit / cambio
+                const newJournal = {
+                    ...restData,
+                    Credit,
+                    Debit: 0,
+                    DebitSys: 0,
+                    CreditSys: parseFloat(newValue.toFixed(2))
+                }
+                journalList.push(newJournal)
+            }
+
+            if(Credit == 0 && Debit !== 0){
+                const newValue = +Debit / cambio
+                const newJournal = {
+                    ...restData,
+                    Credit: 0,
+                    Debit,
+                    DebitSys: parseFloat(newValue.toFixed(2)),
+                    CreditSys: 0
+                }
+                journalList.push(newJournal)
+            }
+        })
+        
+        const data = {
+            ReferenceDate,
+            DueDate,
+            Memo,
+            Reference,
+            Reference2,
+            Reference3,
+            JournalEntryLines:journalList, 
+        }
+        console.log('final data -----------------------------------------------')
+        console.log({data})
+        return res.json({data})
         const response = await asientoContable({
             ReferenceDate,
             DueDate,
@@ -21,6 +76,8 @@ const asientoContableController = async (req, res) => {
             JournalEntryLines
         })
 
+        const status = response.status
+        if (!status) return res.status(400).json({ mensaje: 'Hubo un error al guardar el asiento contable' })
         return res.json({ response })
     } catch (error) {
         return res.status(500).json({ mensaje: 'Error en asientoContableController no controlado' })
---
+++ src/web/contabilidad_module/controllers/hana.controller.js
@@ -0,0 +1,63 @@
+const hana = require('@sap/hana-client');
+
+// Configura la conexión a la base de datos HANA
+const connOptions = {
+    serverNode: `${process.env.HANASERVER}:${process.env.HANAPORT}`,
+    uid: process.env.HANAUSER,
+    pwd: process.env.HANAPASS
+};
+
+// Variable para almacenar la conexión a la base de datos
+let connection = null;
+
+// Función para conectar a la base de datos HANA
+const connectHANA = () => {
+    return new Promise((resolve, reject) => {
+        connection = hana.createConnection();
+        connection.connect(connOptions, (err) => {
+            if (err) {
+                console.error('Error de conexión a HANA:', err.message);
+                reject(err);
+            } else {
+                console.log('Conectado a la base de datos HANA');
+                resolve(connection);
+            }
+        });
+    });
+};
+
+
+const executeQuery = async (query) => {
+    return new Promise((resolve, reject) => {
+        console.log(query)
+        connection.exec(query, (err, result) => {
+            if (err) {
+                console.log('error en la consulta:', err.message)
+                reject(new Error('error en la consulta'))
+            } else {
+                console.log('Datos obtenidos con exito');
+                resolve(result);
+                // console.log({result})
+            }
+        })
+    })
+}
+
+const tipoDeCambion = async ()=>{
+    try {
+        if (!connection) {
+            await connectHANA();
+        }
+        console.log('tipoDeCambion EXECUTE')
+        const query = `CALL "LAB_IFA_PRD".IFA_CON_MONEDAS_TIPOS();`
+        const result = await executeQuery(query)
+        return result
+    } catch (error) {
+        console.log({error})
+        throw new Error('error en tipoDeCambion')
+    }
+}
+
+module.exports = {
+    tipoDeCambion
+}
