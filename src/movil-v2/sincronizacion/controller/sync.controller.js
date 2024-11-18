7ee45af5b115e295432e494965235274f7cce320
Gino Baptista <gb.g@live.com>
2024-11-18 15:07:14 -0400

    movil v2 correciones

--- src/movil-v2/sincronizacion/controller/sync.controller.js
+++ src/movil-v2/sincronizacion/controller/sync.controller.js
@@ -4,6 +4,7 @@ const sync = async (req, res) => {
     try {
         const { name } = req.body
         const clientes = await findClientePorVendedor(name)
+        if(clientes.length == 0) return res.status(400).json({mensaje:'el vendedor no tiene clientes'})
         const descuentosArticulos = await findDescuentosArticulos()
         const descuentosCondicion = await findDescuentosCondicion()
         const descuentosLineas = await findDescuentosLineas()
@@ -19,6 +20,7 @@ const moraClienteController = async (req, res) => {
     try {
         const { cardCode } = req.body
         const response = await moraCliente(cardCode)
+        if(response.length == 0) return res.status(404).json({mensaje:'no existe el cliente'})
         return res.json({ response })
     } catch (error) {
         console.log({ error })
