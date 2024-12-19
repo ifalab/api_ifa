1c220770d774052674a5afdcaa742f4d5242033b
Gino Baptista <gb.g@live.com>
2024-12-19 09:22:29 -0400

    modulo pedido - saldo disponible

--- src/web/pedido_module/controller/pedido.controller.js
+++ src/web/pedido_module/controller/pedido.controller.js
@@ -1,17 +1,30 @@
+const { response } = require("express")
 const { findClientePorVendedor } = require("./hana.controller")
 
 const clientesVendedorController = async (req, res) => {
     try {
-
+        
         const { name } = req.body
-        const clientes = await findClientePorVendedor(name)
+        const response = await findClientePorVendedor(name)
+        
+        let clientes = []
+
+        response.map((item) => {
+            const { CreditLine, AmountDue, ...restCliente } = item
+            const saldoDisponible = (+CreditLine) - (+AmountDue)
+            const newData = {
+                ...restCliente,
+                CreditLine,
+                AmountDue,
+                saldoDisponible
+            }
+            clientes.push({...newData})
+        })
         return res.json({ clientes })
 
     } catch (error) {
-        
         console.log({ error })
         return res.status(500).json({ mensaje: 'error en el controlador' })
-
     }
 }
 
