a79e0a7fd656610413a1e2186ad2554e803a2e29
melifisher <melifisher1234@gmail.com>
2025-04-09 11:36:50 -0400

    facturas genesis

---
+++ src/web/inventarios/controller/sql_genesis.controller.js
@@ -0,0 +1,35 @@
+const { sql, poolGenesisPromise } = require('../service/sqlGenesis')
+
+async function getFacturasParaDevolucion(cardCode, itemCode, lote) {
+    try {
+        const pool = await poolGenesisPromise;
+        const result = await pool.request()
+            .input('I_CardCode', sql.VarChar(50), cardCode)
+            .input('I_ItemCode', sql.VarChar(50), itemCode)
+            .input('I_Lote', sql.VarChar(50), lote)
+            .execute('spVentaCliente');
+        console.log('Resultados:', result);
+        return result.recordset
+    } catch (err) {
+        console.error('Error en la consulta getFacturasParaDevolucion:', err);
+        return {message: `Error en la consulta getFacturasParaDevolucion: ${err.message}`}
+    }
+}
+
+async function getDetalleFacturasParaDevolucion(nro_cuenta) {
+    try {
+        const pool = await poolGenesisPromise;
+        console.log({pool})
+        const result = await pool.request().query(`exec spDetalleVenta @I_NroCuenta =${nro_cuenta}`);
+        console.log('Resultados:', result.recordset);
+        return result.recordset
+    } catch (err) {
+        console.error('Error en la consulta getDetalleFacturasParaDevolucion:', err);
+        return {message: `Error en la consulta getDetalleFacturasParaDevolucion: ${err.message}`}
+    }
+}
+
+module.exports = {
+    getFacturasParaDevolucion,
+    getDetalleFacturasParaDevolucion
+}
