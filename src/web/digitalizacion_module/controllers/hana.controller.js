02fd821d1361480a80be8a9896d7e8b256b30471
muimui69 <moises_c2015@outlook.es>
2025-06-18 12:47:44 -0400

    feat: digitalizacion module

--- src/web/digitalizacion_module/controllers/hana.controller.js
+++ src/web/digitalizacion_module/controllers/hana.controller.js
@@ -7,13 +7,13 @@ const reporteEntregaDigitalizacion = async (startDate,endDate,search,skip,limit)
     let query;
     // Manejar correctamente los casos donde startDate o endDate son nulos
     if (!startDate && !endDate) {
-      query = `CALL "LAB_IFA_LAPP"."IFASP_LAPP_GET_DELIVERY_DIGITALIZED"(NULL, NULL, '${search}', ${skip}, ${limit});`;
+      query = `CALL "LAB_IFA_LAPP"."IFASP_LAPP_GET_DELIVERY_PENDING_TO_DIGITALIZED"(NULL, NULL, '${search}', ${skip}, ${limit});`;
     } else if (!startDate) {
-      query = `CALL "LAB_IFA_LAPP"."IFASP_LAPP_GET_DELIVERY_DIGITALIZED"(NULL, '${endDate}', '${search}', ${skip}, ${limit});`;
+      query = `CALL "LAB_IFA_LAPP"."IFASP_LAPP_GET_DELIVERY_PENDING_TO_DIGITALIZED"(NULL, '${endDate}', '${search}', ${skip}, ${limit});`;
     } else if (!endDate) {
-      query = `CALL "LAB_IFA_LAPP"."IFASP_LAPP_GET_DELIVERY_DIGITALIZED"('${startDate}', NULL, '${search}', ${skip}, ${limit});`;
+      query = `CALL "LAB_IFA_LAPP"."IFASP_LAPP_GET_DELIVERY_PENDING_TO_DIGITALIZED"('${startDate}', NULL, '${search}', ${skip}, ${limit});`;
     } else {
-      query = `CALL "LAB_IFA_LAPP"."IFASP_LAPP_GET_DELIVERY_DIGITALIZED"('${startDate}', '${endDate}', '${search}', ${skip}, ${limit});`;
+      query = `CALL "LAB_IFA_LAPP"."IFASP_LAPP_GET_DELIVERY_PENDING_TO_DIGITALIZED"('${startDate}', '${endDate}', '${search}', ${skip}, ${limit});`;
     }
     return await executeQueryWithConnection(query);
   } catch (error) {
