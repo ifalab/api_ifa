806f87d6c5e319ecc75462f27b5e61b211c5d947
German <87827345+germancsaid@users.noreply.github.com>
2025-10-01 13:01:59 -0400

    feat: sld import

--- src/web/importacion_module/controller/importacion.controller.js
+++ src/web/importacion_module/controller/importacion.controller.js
@@ -4,7 +4,7 @@ const { obtenerimportacionStatus } = require("./hana.controller");
 const importacionStatusController = async (req, res) => {
     try {
         const data = await obtenerimportacionStatus()
-        return res.json({ data })
+        return res.json(data)
 
     } catch (error) {
         const usuario = req.usuarioAutorizado || { USERCODE: 'Desconocido', USERNAME: 'Desconocido' }
@@ -16,6 +16,97 @@ const importacionStatusController = async (req, res) => {
 }
 
 
+
+const createReserveInvoiceControllerok = async (req, res) => {
+    try {
+        return res.status(500).json({ mensaje: 'Error en el proceso en el controlador' })
+        return res.json({
+            message: 'Se proceso correctamente'
+        })
+
+
+
+        const data = await obtenerimportacionStatus()
+        return res.json({ data })
+
+
+
+    } catch (error) {
+        const usuario = req.usuarioAutorizado || { USERCODE: 'Desconocido', USERNAME: 'Desconocido' }
+        grabarLog(usuario.USERCODE, usuario.USERNAME, "Importaciones Status", `${error.message || 'Error al obtener impotationStatus'}`, `importacion status controller`, "importacion-status", process.env.PRD)
+        console.log('error en importacionStatusController')
+        console.log({ error })
+        return res.status(500).json({ mensaje: 'Error en el controlador' })
+    }
+}
+
+
+const createReserveInvoiceController = async (req, res) => {
+    try {
+        // 1. Obtener id del body
+        const docentry = req.query.DocEntry;
+
+        if (!docentry) {
+            return res.status(400).json({ mensaje: 'DocEntry es requerido en el query' });
+        }
+
+        // 2. Obtener detalle de la db
+        const data = await obtenerDetalleParaResInvoice(docentry);
+
+        if (!data || data.length === 0) {
+            return res.status(404).json({ mensaje: 'No se encontraron datos para el DocEntry proporcionado' });
+        }
+
+        // 3. Procesar la informacion para mandar al service layer
+        // transformar o formatear al body que resuve el service la
+        const firstItem = data[0];
+
+        const formattedData = {
+            DocDate: new Date(firstItem.DocDate).toISOString().split('T')[0],
+            CardCode: firstItem.CardCode,
+            DocCurrency: firstItem.DocCurrency,
+            DocTotalFc: parseFloat(firstItem.DocTotalFC),
+            ReserveInvoice: firstItem.ReserveInvoice,
+            PriceMode: firstItem.PriceMode,
+            DocumentLines: data.map(item => ({
+                LineNum: item.LineNum,
+                ItemCode: item.ItemCode,
+                Quantity: parseFloat(item.Quantity),
+                WarehouseCode: item.WarehouseCode,
+                BaseType: item.BaseType,
+                BaseEntry: item.BaseEntry,
+                BaseLine: item.BaseLine
+            }))
+        };
+
+        // 4. datos formateados  mandar al service layer
+        // postReserveInvoice
+        // const serviceResponse = await postReserveInvoice(formattedData);
+
+        // 6. Manejar la respuesta del service layer (in this case, the data from obtenerimportacionStatus)
+        // if (!serviceResponse) {
+        //     return res.status(500).json({ mensaje: 'Error en el service layer' });
+        // }
+
+        // 8. Responder al cliente con el resultado
+        return res.json({
+            message: 'Factura de reserva creada exitosamente',
+            data: formattedData
+        });
+
+    } catch (error) {
+        const usuario = req.usuarioAutorizado || { USERCODE: 'Desconocido', USERNAME: 'Desconocido' };
+        grabarLog(usuario.USERCODE, usuario.USERNAME, "Import Crear Factura de reserva", `${error.message || 'Error al crear factura de reserva de importaciones'}`, `createReserveInvoiceController`, "create-reserve-invoice", process.env.PRD);
+        console.log('error en createReserveInvoiceController');
+        console.log({ error });
+        return res.status(500).json({ mensaje: 'Error en el controlador' });
+    }
+}
+
+
+
 module.exports = {
-   importacionStatusController
+   importacionStatusController,
+   createReserveInvoiceController,
+   createReserveInvoiceControllerok
 }
--- src/web/importacion_module/router/importacion.routes.js
+++ src/web/importacion_module/router/importacion.routes.js
@@ -2,10 +2,14 @@ const { Router } = require('express')
 const { validarToken } = require('../../../middleware/validar_token.middleware')
 const { validarCampos } = require('../../../middleware/validar_campos.middleware')
 const {
-     importacionStatusController
+     importacionStatusController,
+     createReserveInvoiceControllerok,
+     createReserveInvoiceController
 } = require('../controller/importacion.controller')
 const router = Router()
 
 router.get('/importacion-status', [validarToken, validarCampos], importacionStatusController)
+router.post('/create-reserve-invoice', [validarToken, validarCampos], createReserveInvoiceControllerok)
+router.post('/create-reserve-invoicetest', [validarToken, validarCampos], createReserveInvoiceController)
 
 module.exports = router
